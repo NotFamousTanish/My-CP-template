3d602f541c948d5ec2bde9c58ca4ac1c96e30d85
Tanish Sharma <109678180+tanish5nov@users.noreply.github.com>
2024-01-25 12:28:33 +0530

    segment_tree.cpp

--- segment_tree.cpp
+++ segment_tree.cpp
@@ -1,53 +1,49 @@
-//left child index  = 2*x + 1
-//right child index = 2*x + 2
-//x is the index of parent element
-//mid element = (l+r)/2;
-//left segment = l to mid-1
-//right segment = mid to r-1
-//leaf nodes are the parent array elements
-//so no(leaf nodes) = size of parent array
-//range can be of three condns
-//1. no overlaps
-//2. overlaps
-//3. partial overlaps
-//to make segtree-> o(n)
-//to query-> o(log n)
-
-int a[(int)1e5], seg[4* (int)1e5];
-
-void build(int ind, int low, int high){
-    if(low == high){
-        seg[ind] = a[low];
-        return;
-    }
-    int mid = (low+high)/2;
-    build(2*ind +1, low, mid);
-    build(2*ind +2, mid+1, high);
-
-    //whaterver you want to do
-    seg[ind] = max(seg[2*ind+1], seg[2*ind+2]);
-}
-
-int query(int ind, int low, int high, int l, int r){
-    // l and r are taken here to check if my range overlaps or not or is it completely outside
-    if(low >= l && high <= r){
-        return seg[ind];
+struct segtree{
+    int size;
+    vector<int>sums;
+    void init(int n){
+        size = 1;
+        while(size <= n){
+            size*=2;
+        }
+        sums.assign(2 * size , 0ll);
     }
 
-    if(high < l || low > r){
-        // answer does not lies
-        // return as required
-        return INT_MIN;
+    void set(int i, int v, int x, int lx, int rx){
+        if(rx - lx  == 1){
+            sums[x] = v;
+            return;
+        }
+        int mid = (lx+rx)/2;
+        if(i < mid){
+            set(i, v, 2 * x + 1, lx, mid);
+        }else{
+            set(i, v, 2 * x + 2, mid, rx);
+        }
+
+        sums[x] = sums[2 * x + 1] + sums[2 * x + 2];
     }
-    int left = query(2*ind+1, low, mid, l , r);
-    int right = query(2*ind+2, mid+1, high, l , r);
 
-    //return as required here
-    return max(left, right);
-}
+    void set(int i, int v){
+        set(i , v, 0, 0, size);
+    }
 
+    long long sum(int l, int r, int x, int lx, int rx){
+        if(lx >= r || l >= rx){
+            return 0;
+        }
+        if(lx >= l && rx <= r){
+            return sums[x];
+        }
+        int mid = (lx + rx)/2;
 
+        int sl = sum(l, r, 2 * x + 1, lx, mid);
+        int sr = sum(l, r, 2 * x + 2, mid, rx);
 
+        return sl + sr;
+    }
 
-
-
+    long long sum(int l , int r){
+        return sum(l, r, 0, 0, size);
+    }
+};
