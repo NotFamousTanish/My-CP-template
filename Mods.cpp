431358ded50986ed96158207bdb10ed91e6d8485
Tanish Sharma <109678180+tanish5nov@users.noreply.github.com>
2024-04-05 00:20:48 +0530

    Update Mods.cpp

--- Mods.cpp
+++ Mods.cpp
@@ -1,25 +1,21 @@
-// int should be defined as long long
+const int MOD = 1e9+7;
 int mod(int x, int m){
     return (x%m + m)%m;
 }
 int modMul(int a, int b, int m) {
-     a = a % m;
-     b = b % m;
+     a = mod(a,m);
+     b = mod(b,m);
      return ((a * b) % m);
 }
 int modAdd(int a, int b, int m) {
-     a %= m;
-     b %= m;
+     a = mod(a,m);
+     b = mod(b,m);
      return ((a + b) % m);
 }
 int modSub(int a, int b, int m) {
-     a %= m;
-     b %= m;
+     a = mod(a,m);
+     b = mod(b,m);
      return ((a - b + m) % m);
-}
-int invMod(int a, int m){
-    //(1/a)%m = a^(m-2)
-    return powl(a, m-2);
 }
 int modPow(int x, int y, int m){
     if(y==0){
@@ -32,4 +28,27 @@ int modPow(int x, int y, int m){
     }else{
         return modMul(ans1, ans2, m);
     }
+}
+int invMod(int a, int m){
+    //(1/a)%m = a^(m-2)
+    return modPow(a, m-2, m);
+}
+int modDiv(int a, int b, int m){
+    return modMul(a, invMod(b,m), m);
+}
+ 
+const int factN = 2e6+5;
+int fact[factN];
+void preFact(int m){
+    fact[0] = 1;
+    for(int i = 1; i<=factN; i++){
+        fact[i] = modMul(fact[i-1], i, m);
+    }
+}
+ 
+int ncr(int n, int r, int m){
+    int den = modMul(fact[r], fact[n-r], MOD);
+    int num = fact[n];
+    int ans = modDiv(num, den, m);
+    return ans;
 }
