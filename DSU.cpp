317aa14b6ae30e2112d3bb005c2b6c4c198317f3
Tanish Sharma <109678180+tanish5nov@users.noreply.github.com>
2024-07-07 18:37:11 +0530

    Create DSU(disjoint-set-union-union-find).cpp

---
+++ DSU.cpp
@@ -0,0 +1,25 @@
+int parent[N];
+int size[N] = {0};
+void make(int node){
+    parent[node] = node;
+    size[node] = 1;
+}
+
+int find(int node){
+    if(parent[node] == node){
+        return node;
+    }
+    return parent[node] = find(parent[node]);
+}
+
+void Union(int node1, int node2){
+    node1 = find(node1);
+    node2 = find(node2);
+    if(node1 != node2){
+        if(size[node1] < size[node2]){
+            swap(node1, node2);
+        }
+        parent[node2] = node1;
+        size[node1]+=size[node2];
+    }
+}
